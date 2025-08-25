6a116a03681c64db73973d20c1f79cb146f56a65
jcristancho2 <jocristanchool@gmail.com>
2025-08-25 11:28:05 -0500

    feat: :sparkles: creacion de modulo de usuario

---
+++ Src/Modules/User/Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace campuslovejorge_edwin.Src.Modules.User.Domain.Entities;
+
+public class User {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string profession { get; set; }
+    public string Interests { get; set; }
+    public int DailyLikes { get; set; }
+}
